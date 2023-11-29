b171b1200d12ba38fc856488e5f228169a60e3bb
Madli Reiljan <122225818+Hecate101@users.noreply.github.com>
2023-11-29 09:37:04 +0000

    Quiz

---
+++ script.js
@@ -0,0 +1,75 @@
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false},
+      { text: "Blue whale", correct: true},
+      { text: "Elephant", correct: false},
+      { text: "Giraffe", correct: false},
+    ]
+  },
+  {
+    question: "Which is the largest desert in the world?",
+    answers: [
+      { text: "Kalahari", correct: false},
+      { text: "Gobi", correct: false},
+      { text: "Sahara", correct: false},
+      { text: "Antarctica", correct: true},
+    ]
+  },
+  {
+    question: "Which is the smallest continent in the world?",
+    answers: [
+      { text: "Asia", correct: false},
+      { text: "Australia", correct: true},
+      { text: "Arctic", correct: false},
+      { text: "Africa", correct: false},
+    ]
+  },
+  {
+    question: "Which is the smallest country in the world?",
+    answers: [
+      { text: "Vatican City", correct: true},
+      { text: "Bhutan", correct: false},
+      { text: "Nepal", correct: false},
+      { text: "Shri Lanka", correct: false},
+    ]
+  }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndext = 0;
+let score = 0;
+
+function startQuiz(){
+  currentQuestionIndext = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion(){
+  resetState();
+  let currentQuestion = questions[currentQuestionIndext];
+  let questionNo = currentQuestionIndext + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+  })
+}
+
+function resetState(){
+  nextButton.style.display = "none";
+  while(answerButtons.firstCHild){
+    answerButtons.removeCHild(answerButtons.firstCHild);
+  }
+}
+
+startQuiz();
